894b96e49e0a4db0fcc34601e20372ee0ad77756
ChristianKern97 <ikerneii97@gmail.com>
2021-11-21 01:56:44 +0100

    Some initial Sailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,27 @@ module.exports = {
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            forkBlue: {
+                dark: '#151622',
+                DEFAULT: '#1D2030',
+                light: '#292C3D'
+            },
+            label: {
+                DEFAULT: '#626684',
+                hover: '#E2E3E9',
+                selected: '#E2E3E9',
+            }
+        },
         extend: {},
     },
     variants: {
-        extend: {},
+        extend: {
+            backgroundColor: ['active'],
+            textColor: ['active']
+        },
     },
     plugins: [],
 }
