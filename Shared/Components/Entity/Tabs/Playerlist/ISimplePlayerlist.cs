ab5b177c8f1a6686976afa56397ef3facfde2638
ChristianKern97 <ikerneii97@gmail.com>
2021-12-28 15:02:19 +0100

    Added Banlist and Whitelist
    fixed #17

---
+++ Shared/Components/Entity/Tabs/Playerlist/ISimplePlayerlist.cs
@@ -0,0 +1,9 @@
+﻿using ProjectAveryCommon.Model.Entity.Pocos.Player;
+
+namespace ProjectAveryFrontend.Shared.Components.Entity.Tabs.Playerlist;
+
+public interface ISimplePlayerlist
+{
+    public Player? ActivePlayer { get; set; }
+    public void SelectPlayer(PlayerComponent playerComponent);
+}
